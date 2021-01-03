e65c3562b12fe07d7fe67c721e2d06354e1a8618
bmilasauskas <benas@zenitech.co.uk>
2021-01-03 15:16:39 +0200

    special items logic implemented

--- src/special_item_logic.js
+++ src/special_item_logic.js
@@ -1,22 +1,45 @@
 const apply_special_item_rules = (item) => {
   const name = item.name.toString().trim().toLowerCase()
+  let result = null
   Object.keys(actions).forEach((key) => {
     if(name.includes(key)) {
-      actions[key](item)
+      result = actions[key](item)
     }
   })
-  return item
+  return result !== null ? result : item
 }
 
 const actions = {
-  'aged brie': () => {
-    console.log("briiiiiiiiiii")
+  'aged brie': (item) => {
+    //There was no information given if quality of brie increase twice faster when sellIn is <= 0, so left default of +1 daily
+    const quality = parseInt(item.quality) < 50 ? parseInt(item.quality) + 1 : 50
+    const sellIn = parseInt(item.sellIn) > 0 ? parseInt(item.sellIn) - 1 : 0
+    item.quality = quality.toString()
+    item.sellIn = sellIn.toString()
+    return item
   },
-  'backstage passes': () => {
-    console.log("baaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaack")
+  'backstage passes': (item) => {
+    let sellIn = parseInt(item.sellIn)
+    let quality = parseInt(item.quality)
+
+    if(sellIn > 10) quality += 1
+    if(sellIn <= 10 && sellIn > 5) quality += 2
+    if(sellIn <= 5 && sellIn > 0) quality += 3
+    if(sellIn <= 0) quality = 0
+
+    item.sellIn = parseInt(item.sellIn) > 0 ? sellIn - 1 : 0
+    item.quality = quality > 50 ? 50 : quality
+    return item
   },
-  'conjured': () => {
-    console.log("conjuuuuuuuuuuuuuuuuuuuuuuuurs")
+  'conjured': (item) => {
+    let quality = parseInt(item.quality)
+    let sellIn = parseInt(item.sellIn)
+    if(sellIn > 0) quality -= 2
+    if(sellIn <= 0) quality -= 4
+    sellIn -= 1
+    item.quality = quality < 0 ? 0 : quality
+    item.sellIn = sellIn < 0 ? 0 : sellIn
+    return item
   }
 }
 
--- src/transform_streams.js
+++ src/transform_streams.js
@@ -6,8 +6,8 @@ const SPECIAL_ITEMS = ['sulfuras', 'aged brie', 'backstage passes', 'conjured']
 
 const special_items_transform = new Transform({
   transform(chunk, encoding, callback) {
-    apply_special_item_rules(JSON.parse(chunk))
-    this.push(chunk)
+    const result = apply_special_item_rules(JSON.parse(chunk))
+    this.push(JSON.stringify(result))
     callback()
   }
 })
@@ -28,15 +28,14 @@ const daily_transform = new Transform({
     if(!SPECIAL_ITEMS.some(str => name.includes(str))) {
       performDailyReduction(item)
     }
-
     this.push(JSON.stringify(item))
     callback()
   }
 })
 
 const performDailyReduction = (item) => {
-  let quality = parseInt(item.quality.toString())
-  let sellIn = parseInt(item.sellIn.toString())
+  let quality = parseInt(item.quality)
+  let sellIn = parseInt(item.sellIn)
 
   if(sellIn > 0) {
     sellIn -= 1
