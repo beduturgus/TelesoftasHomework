d6d3af2b47e41b156b7be83351c8db983ed1b76f
bmilasauskas <benas@zenitech.co.uk>
2021-01-03 13:34:35 +0200

    daily transformation implementation, skeleton for special items transformation

--- test/texttest_fixture.js
+++ test/texttest_fixture.js
@@ -1,30 +1,15 @@
 const {InputGenerator} = require("../src/input_generator");
-const transform_streams = require("../src/transform_streams")
-
-// const { Shop, Item } = require("../src/gilded_rose");
-// const items = [
-//   new Item("+5 Dexterity Vest", 10, 20),
-//   new Item("Aged Brie", 2, 0),
-//   new Item("Elixir of the Mongoose", 5, 7),
-//   new Item("Sulfuras, Hand of Ragnaros", 0, 80),
-//   new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-//   new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-//   new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-//   new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-//
-//   // This Conjured item does not work properly yet
-//   new Item("Conjured Mana Cake", 3, 6),
-// ];
-
-
-
-
+const {object_mapping_transform, daily_transform, special_items_transform} = require("../src/transform_streams")
 
 const days = Number(process.argv[2]) || 2;
 const inputGenerator = new InputGenerator()
 
-// console.log("OMGHAI!");
-inputGenerator.pipe(transform_streams.day_transform).pipe(process.stdout)
+
+inputGenerator
+.pipe(object_mapping_transform)
+.pipe(daily_transform)
+.pipe(special_items_transform)
+.pipe(process.stdout)
 // for (let day = 0; day < days; day++) {
   // console.log(`\n-------- day ${day} --------`);
   // console.log("name, sellIn, quality");
